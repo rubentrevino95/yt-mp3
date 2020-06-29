c8a3bc04f3f2a46c0e2920e76af0fa68deb5eb53
Ruben <rubentrevino95@gmail.com>
2020-06-28 21:53:37 -0500

    fixed key

--- custom.js
+++ custom.js
@@ -5,7 +5,7 @@ var settings = {
     "method": "GET",
     "headers": {
         "x-rapidapi-host": "coolguruji-youtube-to-mp3-download-v1.p.rapidapi.com",
-        "x-rapidapi-key": "fa3b038dc0mshbfd8a8c34c8527ep1c8393jsnc83257c2f2b3"
+        "x-rapidapi-key": ytToken
     }
 };
 
